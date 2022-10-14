6cef0d4fe13738c334111c2a86486d5a4aa2633a
spicylemon <3093621834@qq.com>
2022-10-14 11:45:42 +0800

    feat: 中途切换less

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,31 @@
 import s from './style.module.less'
+
 function Banner() {
-  return <div className={s.bannerBox}>Banner</div>
+  return (
+    <div className={s.bannerBox}>
+      <div className={s.container}>
+        <form action="">
+          <h1>Web3时代的数字内容分发和创作激励平台。</h1>
+          <p>
+            如果您是漫画作者且希望通过Web3的⽅式帮助您的作品进⾏商
+            业变现，欢迎留下您的联系⽅式(⼿机/微信/QQ)
+          </p>
+          <div className={s.emailBox}>
+            <i className="fas fa-envelope">@</i>
+            <input
+              className={s.tbox}
+              type="email"
+              name=""
+              placeholder="Enter your email"
+            />
+            <button className={s.btn} type="button" name="button">
+              Subscribe
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Banner
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,72 @@
-import { useEffect } from 'react'
 import { useRef } from 'react'
+import { useEffect } from 'react'
+// import { useRef } from 'react'
 import AboutWe from '../../components/AboutWe'
 import Banner from '../../components/Banner'
 import Mission from '../../components/Mission'
-
+import s from './style.module.less'
 function Home() {
   // 拿到各自的 dom 如果到了那个区域则切换背景图或者做对应的操作
-
-  // 每次scoll事件拿到所有的组件，符合条件的更新对应的背景图
-  // 条件顶部到达对应位置范围内则更新
-  // 更新什么，更新对应的背景图，所以每个组件需要一个自己的背景图
-  const banner = {
-    dom: useRef(null),
+  const home = useRef(null)
+  const callBackArr = [
+    (item) => {
+      const homeDom = home.current
+      if (homeDom) {
+        homeDom.style.backgroundColor = '#F8EDE3'
+        item.style.opacity = 1
+      }
+    },
+    (item) => {
+      const homeDom = home.current
+      if (homeDom) {
+        homeDom.style.backgroundColor = '#7D6E83'
+        item.style.opacity = 1
+      }
+    },
+    (item) => {
+      const homeDom = home.current
+      if (homeDom) {
+        homeDom.style.backgroundColor = 'rgb(238, 199, 180)'
+        item.style.opacity = 1
+      }
+    },
+  ]
+  const monitorFn = () => {
+    // 更新时机
+    // 1、dom顶部距离窗口顶部距离在范围内
+    // 2、dom底部距离窗口底部距离在范围内
+    const homeCards = Array.from(document.getElementsByClassName('homeCard'))
+    homeCards.forEach((item, index) => {
+      const gap = 300
+      const { top, bottom } = item.getBoundingClientRect()
+      if ((top < gap && top >= 0) || (bottom > gap && bottom < gap + 50)) {
+        callBackArr[index](item)
+      }
+    })
   }
   useEffect(() => {
-    console.log(banner.dom.current)
+    // console.log(banner.dom.current)
+    monitorFn()
+    window.addEventListener('scroll', monitorFn)
   }, [])
   return (
     <div>
-      <div ref={banner.dom}>
+      <div ref={home} className={s.homeBox}></div>
+      <div className={s.logo}>Digitok</div>
+      <div className={s.arrowsBody}>
+        <div className={s.arrow}></div>
+        <div className={s.arrow}></div>
+        <div className={s.arrow}></div>
+      </div>
+      <div className="homeCard" style={{ opacity: 0, transition: '1s' }}>
         <Banner></Banner>
       </div>
-      <Mission></Mission>
-      <AboutWe></AboutWe>
+      <div className="homeCard" style={{ opacity: 0, transition: '1s' }}>
+        <Mission></Mission>
+      </div>
+      <div className="homeCard" style={{ opacity: 0, transition: '1s' }}>
+        <AboutWe></AboutWe>
+      </div>
     </div>
   )
 }
